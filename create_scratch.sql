6f4ce2662aed5f6137c211b14dae338b5e180cfe
archaean <archaeanksu@gmail.com>
2014-03-02 00:15:08 -0600

    Added classes up to boatman, fixed load script, and added some basic verification queries.

--- create_scratch.sql
+++ create_scratch.sql
@@ -2,17 +2,19 @@ drop table classes_csv;
 
 
 create table classes_csv (class VARCHAR(50), relation VARCHAR(20), 
-                          value1 VARCHAR(50), modifier VARCHAR(50));
+                          value VARCHAR(50), modifier VARCHAR(50));
                           
 truncate table classes_csv;
 
+--MySQL script for loading csv file
 --load has to be run from console?
 load data local infile 'classes.csv' into table classes_csv fields 
 terminated by ','
 enclosed by '"'
-escaped by '//'
 lines terminated by '\n';
 
 
-
-select distinct class from classes_csv;
+select distinct class from classes_csv order by class;
+select distinct relation from classes_csv order by relation;
+select distinct value from classes_csv where relation like '%Skills%' order by value;
+select distinct value from classes_csv where relation like '%Career Exits%' order by value;
