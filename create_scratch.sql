1b90e5bb69a5c0cde45042622e7a166e4b884f71
archaean <archaeanksu@gmail.com>
2014-03-02 14:41:09 -0600

    added up to footpad skipped over a little bit of Entertainer

--- create_scratch.sql
+++ create_scratch.sql
@@ -8,11 +8,14 @@ truncate table classes_csv;
 
 --MySQL script for loading csv file
 --load has to be run from console?
+--$ mysql -u [username] -p --local-infile
+--mysql> use warhammer
 load data local infile 'classes.csv' into table classes_csv fields 
 terminated by ','
 enclosed by '"'
 lines terminated by '\n';
 
+select * from classes_csv;
 
 select distinct class from classes_csv order by class;
 select distinct relation from classes_csv order by relation;
