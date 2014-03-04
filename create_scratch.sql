2ab5785f34febc55b0d40f7283644eae82f7da17
Matthew Hoag <mhoag@kumc.edu>
2014-03-04 12:55:17 -0600

    Added career exits up to student

--- create_scratch.sql
+++ create_scratch.sql
@@ -1,15 +1,16 @@
 drop table classes_csv;
 
-
 create table classes_csv (class VARCHAR(50), relation VARCHAR(20), 
                           value VARCHAR(50), modifier VARCHAR(50));
                           
 truncate table classes_csv;
 
---MySQL script for loading csv file
---load has to be run from console?
---$ mysql -u [username] -p --local-infile
---mysql> use warhammer
+/*
+   MySQL script for loading csv file
+   load has to be run from console?
+   $ mysql -u [username] -p --local-infile
+   mysql> use warhammer   
+*/
 load data local infile 'classes.csv' into table classes_csv fields 
 terminated by ','
 enclosed by '"'
